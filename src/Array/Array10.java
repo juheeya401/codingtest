e3b86b8c7e1fd1c7b17794c7caa10b1e3508b1aa
lee.juhee <lee.juhee@kstm.co.kr>
2022-03-22 19:20:23 +0900

    -

---
+++ src/Array/Array10.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Array10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        personal(scanner);
+    }
+
+    /**
+     * 우선 다 입력받는다
+     * 상,하,좌,우 공식을 찾는다
+     * 전체를 탐색하면서 비교하여 count 한다.
+     * 상:
+     */
+    private static void personal(Scanner scanner) {
+
+    }
+}
