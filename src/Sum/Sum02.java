ebdf88db20751e15184abeadca3cc0198ff2fb5c
lee.juhee <lee.juhee@kstm.co.kr>
2022-04-08 18:08:17 +0900

    Sum02 - 강의듣고 다시풀기 투포인터 문제! 성공!

--- src/Sum/Sum02.java
+++ src/Sum/Sum02.java
@@ -1,14 +1,12 @@
 package Sum;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
- * todo
  * 03-02. 공통원소 구하기
  * 2022-04-07: ❌
+ * 2022-04-08: ⭕️
+ * #TwoPointer
  */
 public class Sum02 {
     public static void main(String[] args) {
@@ -24,20 +22,20 @@ public class Sum02 {
         // 입력
         int aCnt = scanner.nextInt();
         int[] aArr = new int[aCnt];
-        for (int i=0; i<aCnt; i++) {
+        for (int i = 0; i < aCnt; i++) {
             aArr[i] = scanner.nextInt();
         }
         int bCnt = scanner.nextInt();
         int[] bArr = new int[bCnt];
-        for (int i=0; i<bCnt; i++) {
+        for (int i = 0; i < bCnt; i++) {
             bArr[i] = scanner.nextInt();
         }
 
         // 알고리즘
         // a * b 만큼 반복문 돌림 (찾으면 바로 break)
         List<Integer> answer = new ArrayList<>();
-        for (int a=0; a<aCnt; a++) {
-            for (int b=0; b<bCnt; b++) {
+        for (int a = 0; a < aCnt; a++) {
+            for (int b = 0; b < bCnt; b++) {
                 if (aArr[a] == bArr[b]) {
                     answer.add(aArr[a]);
                     break;
@@ -54,23 +52,23 @@ public class Sum02 {
      * 컨셉: 두번째 for 문에서 한 번 찾은 원소는 배열(list)에서 제거했다. -> 반복이 계속될 수록 반복할 갯수가 줄어드는 효과 기대
      * 결과: Time Limit Exceeded
      */
-    private static void personal(Scanner scanner) {
+    private static void personal02(Scanner scanner) {
         // 입력
         int aCnt = scanner.nextInt();
         int[] aArr = new int[aCnt];
-        for (int i=0; i<aCnt; i++) {
+        for (int i = 0; i < aCnt; i++) {
             aArr[i] = scanner.nextInt();
         }
         int bCnt = scanner.nextInt();
         List<Integer> bArr = new LinkedList<>();
-        for (int i=0; i<bCnt; i++) {
+        for (int i = 0; i < bCnt; i++) {
             bArr.add(scanner.nextInt());
         }
 
         // 알고리즘
         List<Integer> answer = new ArrayList<>();
-        for (int a=0; a<aCnt; a++) {
-            for (int b=0; b<bArr.size(); b++) {
+        for (int a = 0; a < aCnt; a++) {
+            for (int b = 0; b < bArr.size(); b++) {
                 if (aArr[a] == bArr.get(b)) {
                     answer.add(aArr[a]);
                     bArr.remove(b); // 추가된 부분 => 한 번 찾은건 제거한다.
@@ -81,5 +79,45 @@ public class Sum02 {
 
         // 출력
         answer.stream().sorted().forEach(e -> System.out.print(e + " "));
+    }
+
+
+    /**
+     * 후) 내가 하기: ⭕️
+     * 컨셉: TwoPointer 를 이용한다. + 두 배열을 정렬한다 => TwoPointer 문제는 정렬이 필요한 것 같다.
+     */
+    private static void personal(Scanner scanner) {
+        // 입력
+        int aCnt = scanner.nextInt();
+        int[] aArr = new int[aCnt];
+        for (int i = 0; i < aCnt; i++) {
+            aArr[i] = scanner.nextInt();
+        }
+        int bCnt = scanner.nextInt();
+        int[] bArr = new int[bCnt];
+        for (int i = 0; i < bCnt; i++) {
+            bArr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(aArr);
+        Arrays.sort(bArr);
+
+        List<Integer> answer = new LinkedList<>();
+        int aP = 0, bP = 0;
+        while (aP < aCnt && bP < bCnt) {
+            int a = aArr[aP];
+            int b = bArr[bP];
+            if (a == b) {
+                answer.add(a);
+                aP++;
+                bP++;
+            } else if (a < b) {
+                aP++;
+            } else {
+                bP++;
+            }
+        }
+
+        answer.stream().forEach(e -> System.out.print(e + " "));
     }
 }
